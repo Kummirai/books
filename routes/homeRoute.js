c0c4f15f71ff775a56582d48fd420b5c08e95ab7
Milton <ajaxmilton@hotmail.com>
2026-01-23 19:38:56 +0200

    fix: update swagger documentation to ignore home route

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -3,6 +3,10 @@ import { homeController } from "../controllers/homeController.js";
 
 const homeRoute = Router();
 
+/**
+ * @swagger
+ *  ignore: true
+ */
 homeRoute.get("/", homeController);
 
 export default homeRoute;
