185051b27b1a11717c5e79db662cb23dd4838d1a
Milton <ajaxmilton@hotmail.com>
2026-01-28 17:22:41 +0200

    feat: update GitHub callback URL and home controller URLs for production deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/github/callback",
+      callbackURL:
+        "https://books-project-xqrb.onrender.com/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       try {
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,8 +4,8 @@ const homeController = async (req, res) => {
       ? `${req.user.displayName}, you are logged in`
       : "You are not logged in",
     project: "Books API",
-    login_url: "http://localhost:3000/auth/github",
-    logout_url: "http://localhost:3000/auth/github/logout",
+    login_url: "https://books-project-xqrb.onrender.com/auth/github",
+    logout_url: "https://books-project-xqrb.onrender.com/auth/github/logout",
     github_url: "https://github.com/Kummirai/books",
     project_api_docs: "https://books-project-xqrb.onrender.com/api-docs",
   });
