85f71f8ed34aac155354756b94b1ef0b08e8a5e8
Milton <ajaxmilton@hotmail.com>
2026-01-28 17:25:41 +0200

    Change logged out message in home controller

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,7 +2,7 @@ const homeController = async (req, res) => {
   return res.status(200).json({
     user: req.user
       ? `${req.user.displayName}, you are logged in`
-      : "You are not logged in",
+      : "You are logged out",
     project: "Books API",
     login_url: "https://books-project-xqrb.onrender.com/auth/github",
     logout_url: "https://books-project-xqrb.onrender.com/auth/github/logout",
